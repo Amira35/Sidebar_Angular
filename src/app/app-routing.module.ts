f141fb76149e01702054a1fad7391d5f4417f661
amira <am1795231@gmail.com>
2023-08-30 01:04:20 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './admin/default/default.component';
+import { DashboardComponent } from './admin/dashboard/dashboard/dashboard.component';
+import { PostsComponent } from './admin/dashboard/posts/posts.component';
+import { CommentsComponent } from './admin/dashboard/comments/comments.component';
+import { PagesComponent } from './admin/dashboard/pages/pages.component';
+import { SettingComponent } from './admin/dashboard/setting/setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '' , component : DefaultComponent , children : [
+
+    {path : 'home' , component : DashboardComponent} ,
+    {path : 'posts' , component : PostsComponent} ,
+    {path : 'comments' , component : CommentsComponent},
+    {path : 'pages' , component : PagesComponent} ,
+    {path : 'setting' , component : SettingComponent} ,
+    {
+      path : '**' , component : DashboardComponent
+    }
+  ]} ,
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { DefaultComponent } from './admin/default/default.component';
+import { SidbarComponent } from './admin/sidbar/sidbar.component';
+import { PostsComponent } from './admin/dashboard/posts/posts.component';
+import { DashboardComponent } from './admin/dashboard/dashboard/dashboard.component';
+import { CommentsComponent } from './admin/dashboard/comments/comments.component';
+import { SettingComponent } from './admin/dashboard/setting/setting.component';
+import { PagesComponent } from './admin/dashboard/pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    SidbarComponent,
+    PostsComponent,
+    DashboardComponent,
+    CommentsComponent,
+    SettingComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule ,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule ,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
